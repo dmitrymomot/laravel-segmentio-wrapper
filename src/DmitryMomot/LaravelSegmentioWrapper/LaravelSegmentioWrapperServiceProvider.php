d300142e43646fabc3259b2816cb89fae51d3c9b
Dmitry Momot <d.momot@mail.synergetica.co>
2015-01-09 12:59:30 +0200

    Was created skeleton of package

---
+++ src/DmitryMomot/LaravelSegmentioWrapper/LaravelSegmentioWrapperServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Dmitrymomot\LaravelSegmentioWrapper;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSegmentioWrapperServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('dmitrymomot/laravel-segmentio-wrapper');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
